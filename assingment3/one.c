f06d590bf4d08b20426c64c2ebfbc3c69fa5e0b8
chandrakant100 <chandrakant.barcelona@gmail.com>
2020-03-03 19:56:45 +0530

    updated

--- assingment3/one.c
+++ assingment3/one.c
@@ -1,73 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
 typedef struct node_create
 {
     char str[100];
     struct node *next;
 }node;
+
 void create(node **f)
 {
-    int i=1;
-    char ch='Y';
-     node *l;
-    while(ch=='Y' || ch=='y')
-    {
+        node *cur=(*f);
         node *c=(node *)malloc(sizeof(node));
-
-        c->next=NULL;
-        printf("Enter the string %d:",i);
+        printf("\nEnter the string");
         fgetc(stdin);
         scanf("%[^\n]",c->str);
-
         if(*f==NULL)
-            *f=c;
-        else
-            l->next=c;
-            l=c;
-        printf("\nDo you want to continue:(y\n):");
-        fgetc(stdin);
-        scanf("%c",&ch);    
-        i++;
-    }
+            {
+                *f=c;
+                return; 
+            }
+        if(strcmp(c->str, cur->str) < 0)
+            {
+                c->next = cur;
+                *f=c;
+                return ;
+            }
+        if(cur->next!=NULL && (strcmp(c->str,cur->next->str) > 0))
+            {
+                cur=cur->next;
+            }
+        c->next=cur->next;
+        cur->next=c;            
+
 }
-void sort(node *f)
-{   
-    char str2[20];
-    memset(str2,'\0',sizeof(str2));
-    node *f1=f,*f2=f;
-    while(f1->next!=NULL)
-    {
-    while(f2->next!=NULL)
-    {
-        if(f1!=f2)
-            if(f2->str[0] < f1->str[0])
-                {
-                    strcpy(str2,f2->str);
-                     strcpy(f2->str,f1->str);
-                    strcpy(f1->str,str2);
-                }
-                f2=f2->next;
-    }
-        f1=f1->next;
-        f2=f;
-    }
-    display(f);
-}
-void display(node *f)
+void display(node *f1)
 {
     printf("\n");
-    while(f!=NULL)
+    while(f1!=NULL)
     {
-        printf("%s\n",f->str);
-        f=f->next;
+        printf("%s\n",f1->str);
+        f1=f1->next;
     }
 }
 int main()
 {
     node *f=NULL;
-    create(&f);
-    sort(f);
-    //display(f);
+    char ch='y';
+    while(ch=='Y' || ch=='y')
+    {
+         create(&f);
+         printf("Do you want to continue(Y/N):");
+         fgetc(stdin);
+         scanf("%c",&ch);
+    }
+    display(f);
     return 0;
 }
