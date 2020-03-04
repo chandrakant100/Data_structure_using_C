84732580b7e9bde148312bed8e9584685844ba3b
chandrakant100 <chandrakant.barcelona@gmail.com>
2020-03-05 01:09:24 +0530

    updated

--- assingment3/two.c
+++ assingment3/two.c
@@ -4,42 +4,41 @@
 
  struct node
 {
-    char str[100];
+    float data;
     struct node *next;
 };
 void create(struct node **f)
 {
         struct node *cur=(*f);
         struct node *c=(struct node *)malloc(sizeof(struct node));
-        printf("\nEnter the string:\n");
-        scanf("%[^\n]",c->str);
-        fgetc(stdin);
+        printf("\nEnter the data:\n");
+        scanf("%f",&c->data);
         if(*f==NULL)
             {
                 *f=c;
                 return; 
             }
-        if(strcasecmp(c->str, cur->str) < 0)
+        if(c->data < cur->data)
             {
                 c->next = cur;
                 *f=c;
                 return ;
             }
-        if(cur->next!=NULL && (strcasecmp(c->str,cur->next->str) > 0))
+        if(cur->next!=NULL && (c->data > cur->next->data))
             {
                 cur=cur->next;
             }
         c->next=cur->next;
-        cur->next=c;         
+        cur->next=c;           
 
 }
 void display(struct node *f1)
 {
     printf("\n");
-    printf("----string after sorting----\n");
+    printf("----Data after sorting in ascending order----\n");
     while(f1!=NULL)
     {
-        printf("%s\n",f1->str);
+        printf("%.2f\n",f1->data);
         f1=f1->next;
     }
 }
@@ -47,12 +46,13 @@ int main()
 {
     struct node *f=NULL;
     char ch='y';
-    while(ch=='Y' || ch=='y')
+    while(ch =='Y' || ch =='y')
     {
          create(&f);
          printf("Do you want to continue(Y/N):");
-         scanf("%c",&ch);
          fgetc(stdin);
+         scanf("%c",&ch);
+            getc(stdin);
     }
     display(f);
     return 0;
